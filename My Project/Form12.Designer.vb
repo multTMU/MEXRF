34d904a6dc3f4c2208cb452f1f30f1730ec91c37
multTMU <71510449+multTMU@users.noreply.github.com>
2022-03-04 17:01:08 -0500

    Add files via upload

---
+++ My Project/Form12.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form12
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form12))
+        Me.txt_watchpath = New System.Windows.Forms.TextBox()
+        Me.btn_startwatch = New System.Windows.Forms.Button()
+        Me.btn_stop = New System.Windows.Forms.Button()
+        Me.txt_folderactivity = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txt_watchpath
+        '
+        Me.txt_watchpath.Location = New System.Drawing.Point(70, 132)
+        Me.txt_watchpath.Name = "txt_watchpath"
+        Me.txt_watchpath.Size = New System.Drawing.Size(577, 20)
+        Me.txt_watchpath.TabIndex = 0
+        '
+        'btn_startwatch
+        '
+        Me.btn_startwatch.Location = New System.Drawing.Point(571, 36)
+        Me.btn_startwatch.Name = "btn_startwatch"
+        Me.btn_startwatch.Size = New System.Drawing.Size(132, 27)
+        Me.btn_startwatch.TabIndex = 1
+        Me.btn_startwatch.Text = "btn_startwatch"
+        Me.btn_startwatch.UseVisualStyleBackColor = True
+        '
+        'btn_stop
+        '
+        Me.btn_stop.Location = New System.Drawing.Point(572, 81)
+        Me.btn_stop.Name = "btn_stop"
+        Me.btn_stop.Size = New System.Drawing.Size(130, 32)
+        Me.btn_stop.TabIndex = 2
+        Me.btn_stop.Text = "stop"
+        Me.btn_stop.UseVisualStyleBackColor = True
+        '
+        'txt_folderactivity
+        '
+        Me.txt_folderactivity.Location = New System.Drawing.Point(70, 205)
+        Me.txt_folderactivity.Name = "txt_folderactivity"
+        Me.txt_folderactivity.Size = New System.Drawing.Size(577, 20)
+        Me.txt_folderactivity.TabIndex = 3
+        '
+        'Form12
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txt_folderactivity)
+        Me.Controls.Add(Me.btn_stop)
+        Me.Controls.Add(Me.btn_startwatch)
+        Me.Controls.Add(Me.txt_watchpath)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form12"
+        Me.Text = "Fit Complete"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt_watchpath As TextBox
+    Friend WithEvents btn_startwatch As Button
+    Friend WithEvents btn_stop As Button
+    Friend WithEvents txt_folderactivity As TextBox
+End Class
---
+++ My Project/Form12.vb
@@ -0,0 +1,71 @@
+﻿Imports System.IO
+Imports System.Diagnostics
+
+Public Class Form12
+    Public watchfolder As FileSystemWatcher
+    Private Sub Form12_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_startwatch.Click
+        watchfolder = New System.IO.FileSystemWatcher()
+
+        'this is the path we want to monitor
+        watchfolder.Path = txt_watchpath.Text
+
+        'Add a list of Filter we want to specify
+        'make sure you use OR for each Filter as we need to
+        'all of those 
+
+        watchfolder.NotifyFilter = IO.NotifyFilters.DirectoryName
+        watchfolder.NotifyFilter = watchfolder.NotifyFilter Or
+                           IO.NotifyFilters.FileName
+        watchfolder.NotifyFilter = watchfolder.NotifyFilter Or
+                           IO.NotifyFilters.Attributes
+
+        ' add the handler to each event
+        AddHandler watchfolder.Changed, AddressOf logchange
+        AddHandler watchfolder.Created, AddressOf logchange
+        AddHandler watchfolder.Deleted, AddressOf logchange
+
+        ' add the rename handler as the signature is different
+        AddHandler watchfolder.Renamed, AddressOf logrename
+
+        'Set this property to true to start watching
+        watchfolder.EnableRaisingEvents = True
+
+        btn_startwatch.Enabled = False
+        btn_stop.Enabled = True
+
+        'End of code for btn_start_click
+    End Sub
+
+    Private Sub logchange(ByVal source As Object, ByVal e As _
+                        System.IO.FileSystemEventArgs)
+        If e.ChangeType = IO.WatcherChangeTypes.Changed Then
+            txt_folderactivity.Text &= "File " & e.FullPath &
+                                    " has been modified" & vbCrLf
+        End If
+        If e.ChangeType = IO.WatcherChangeTypes.Created Then
+            txt_folderactivity.Text &= "File " & e.FullPath &
+                                     " has been created" & vbCrLf
+        End If
+        If e.ChangeType = IO.WatcherChangeTypes.Deleted Then
+            txt_folderactivity.Text &= "File " & e.FullPath &
+                                    " has been deleted" & vbCrLf
+        End If
+    End Sub
+
+    Public Sub logrename(ByVal source As Object, ByVal e As _
+                            System.IO.RenamedEventArgs)
+        txt_folderactivity.Text &= "File" & e.OldName &
+                      " has been renamed to " & e.Name & vbCrLf
+    End Sub
+
+    Private Sub btn_stop_Click(sender As Object, e As EventArgs) Handles btn_stop.Click
+        ' Stop watching the folder
+        watchfolder.EnableRaisingEvents = False
+        btn_startwatch.Enabled = True
+        btn_stop.Enabled = False
+    End Sub
+End Class
